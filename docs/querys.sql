f0f2a0e73300ddb673c9250e412341da3c2469df
Jean Benevenuto <jeanbenev@gmail.com>
2021-04-26 19:09:43 -0400

    Atualização dos arquivos de documentação

--- docs/querys.sql
+++ docs/querys.sql
@@ -2,7 +2,7 @@ CREATE TABLE `sistema-lorem_ipsum`.`projeto`
 ( 
     `id_projeto` INT NOT NULL AUTO_INCREMENT COMMENT 'Id do projeto' ,
     `data_cadastro` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Data de cadastro do registro' ,
-    `nome_projeto` VARCHAR(30) NOT NULL COMMENT 'Nome do projeto' ,
+    `nome_projeto` VARCHAR(60) NOT NULL COMMENT 'Nome do projeto' ,
     `data_inicio` DATE NOT NULL COMMENT 'Data de início' ,
     `data_termino` DATE NOT NULL COMMENT 'Data de término' ,
     `valor_projeto` DOUBLE(20,2) NOT NULL COMMENT 'Valor do projeto',
