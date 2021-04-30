2da7693c1d5cc884129777888c771039ff866ac5
Jean Benevenuto <jeanbenev@gmail.com>
2021-04-30 07:12:17 -0400

    Criação da funcionalidade de debug por function nativa do PHP

--- web/index.php
+++ web/index.php
@@ -9,7 +9,7 @@ require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $config = require __DIR__ . '/../config/web.php';
 
-function dbg($var, $exit = true){
+function dbg($var = "", $exit = true){
     print "<pre>";
     print_r($var);
     print "</pre>";
